3758bb66059d8d604b78cb0a1ce0439c52cdd070
mandrej <milan.andrejevic@gmail.com>
2025-04-16 18:03:09 +0200

    simple grid view

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -1,70 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
-import 'package:photo_view/photo_view_gallery.dart';
 import 'package:provider/provider.dart';
-import 'package:simple_grid/simple_grid.dart';
 import 'api_provider.dart';
 import 'package:flutter_admin_scaffold/admin_scaffold.dart';
 import 'parts/search_form.dart';
-
-class Item {
-  Item({required this.id, required this.record});
-
-  final String id;
-  final Map<String, dynamic> record;
-}
-
-class ItemThumbnail extends StatelessWidget {
-  const ItemThumbnail({
-    super.key,
-    required this.galleryItem,
-    required this.onTap,
-  });
-
-  final Item galleryItem;
-  final GestureTapCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    return GestureDetector(
-      onTap: onTap,
-      child: Hero(
-        tag: galleryItem.id,
-        child: Card(
-          semanticContainer: true,
-          color: Colors.grey.shade200,
-          shadowColor: Colors.transparent,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5.0),
-          ),
-          child: Column(
-            children: [
-              AspectRatio(
-                aspectRatio: 1,
-                child: Image.network(
-                  galleryItem.record['thumb'],
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                child: Text(
-                  galleryItem.record['headline'] ?? '',
-                  overflow: TextOverflow.ellipsis,
-                  style: textTheme.titleMedium?.copyWith(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+import 'parts/simple_grid_view.dart';
 
 class ListPage extends StatefulWidget {
   const ListPage({super.key, required this.title});
@@ -88,12 +27,6 @@ class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
     ApiProvider api = Provider.of<ApiProvider>(context);
-    final records = api.records;
-
-    galleryItems =
-        records.map((record) {
-          return Item(id: record['filename'], record: record);
-        }).toList();
 
     return AdminScaffold(
       appBar: AppBar(
@@ -121,6 +54,7 @@ class _ListPageState extends State<ListPage> {
         items: const [
           AdminMenuItem(title: 'Home', route: '/', icon: Icons.home),
           AdminMenuItem(title: 'Add', route: '/add', icon: Icons.add),
+          AdminMenuItem(title: 'Admin', route: '/admin', icon: Icons.settings),
         ],
         selectedRoute: '/list',
         onSelected: (item) {
@@ -128,137 +62,9 @@ class _ListPageState extends State<ListPage> {
             Navigator.of(context).pushNamed(item.route!);
           }
         },
-        // header: Container(
-        //   height: 50,
-        //   width: double.infinity,
-        //   color: const Color(0xff444444),
-        //   child: const Center(
-        //     child: Text('header', style: TextStyle(color: Colors.white)),
-        //   ),
-        // ),
         header: SearchForm(),
       ),
-      body: SingleChildScrollView(
-        child: SpGrid(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.only(top: 10, bottom: 10),
-          spacing: 10,
-          runSpacing: 10,
-          children:
-              galleryItems.map((item) {
-                return SpGridItem(
-                  xs: 12,
-                  sm: 4,
-                  md: 3,
-                  lg: 2,
-                  child: ItemThumbnail(
-                    galleryItem: item,
-                    onTap: () {
-                      open(context, galleryItems.indexOf(item));
-                    },
-                  ),
-                  // decoration: BoxDecoration(color: Colors.grey[300]),
-                );
-              }).toList(),
-        ),
-      ),
-    );
-  }
-
-  void open(BuildContext context, final int index) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder:
-            (context) => GalleryPhotoViewWrapper(
-              galleryItems: galleryItems,
-              backgroundDecoration: const BoxDecoration(color: Colors.black),
-              initialIndex: index,
-              scrollDirection: Axis.horizontal,
-            ),
-      ),
-    );
-  }
-}
-
-class GalleryPhotoViewWrapper extends StatefulWidget {
-  GalleryPhotoViewWrapper({
-    this.loadingBuilder,
-    this.backgroundDecoration,
-    this.minScale,
-    this.maxScale,
-    this.initialIndex = 0,
-    required this.galleryItems,
-    this.scrollDirection = Axis.horizontal,
-  }) : pageController = PageController(initialPage: initialIndex);
-
-  final LoadingBuilder? loadingBuilder;
-  final BoxDecoration? backgroundDecoration;
-  final dynamic minScale;
-  final dynamic maxScale;
-  final int initialIndex;
-  final PageController pageController;
-
-  final List<Item> galleryItems;
-  final Axis scrollDirection;
-
-  @override
-  State<StatefulWidget> createState() {
-    return _GalleryPhotoViewWrapperState();
-  }
-}
-
-class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
-  late int currentIndex = widget.initialIndex;
-
-  void onPageChanged(int index) {
-    setState(() {
-      currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(icon: const Icon(Icons.delete), onPressed: () {}),
-            Text(
-              widget.galleryItems[currentIndex].record['headline'] ?? '',
-              overflow: TextOverflow.ellipsis,
-              style: Theme.of(
-                context,
-              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
-            ),
-            IconButton(icon: const Icon(Icons.edit), onPressed: () {}),
-          ],
-        ),
-        Expanded(
-          child: PhotoViewGallery.builder(
-            scrollPhysics: const BouncingScrollPhysics(),
-            builder: _buildItem,
-            itemCount: widget.galleryItems.length,
-            loadingBuilder: widget.loadingBuilder,
-            backgroundDecoration: widget.backgroundDecoration,
-            pageController: widget.pageController,
-            onPageChanged: onPageChanged,
-            scrollDirection: widget.scrollDirection,
-          ),
-        ),
-      ],
-    );
-  }
-
-  PhotoViewGalleryPageOptions _buildItem(BuildContext context, int index) {
-    final Item item = widget.galleryItems[index];
-    return PhotoViewGalleryPageOptions(
-      imageProvider: NetworkImage(item.record['url'] as String),
-      initialScale: PhotoViewComputedScale.contained,
-      minScale: PhotoViewComputedScale.contained,
-      maxScale: 1, //PhotoViewComputedScale.covered * 4.1,
-      heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+      body: SimpleGridView(records: api.records),
     );
   }
 }
---
+++ lib/parts/simple_grid_view.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:simple_grid/simple_grid.dart';
+
+class SimpleGridView extends StatelessWidget {
+  SimpleGridView({super.key, required this.records});
+  final List<Map<String, dynamic>> records;
+  late final List<Item> galleryItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final galleryItems =
+        records.map((record) {
+          return Item(id: record['filename'], record: record);
+        }).toList();
+
+    return SingleChildScrollView(
+      child: SpGrid(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.only(top: 10, bottom: 10),
+        spacing: 10,
+        runSpacing: 10,
+        children:
+            galleryItems.map((item) {
+              return SpGridItem(
+                xs: 12,
+                sm: 4,
+                md: 3,
+                lg: 2,
+                child: ItemThumbnail(
+                  galleryItem: item,
+                  onTap: () {
+                    open(context, galleryItems.indexOf(item));
+                  },
+                ),
+                // decoration: BoxDecoration(color: Colors.grey[300]),
+              );
+            }).toList(),
+      ),
+    );
+  }
+
+  void open(BuildContext context, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+            (context) => GalleryPhotoViewWrapper(
+              galleryItems: galleryItems,
+              backgroundDecoration: const BoxDecoration(color: Colors.black),
+              initialIndex: index,
+              scrollDirection: Axis.horizontal,
+            ),
+      ),
+    );
+  }
+}
+
+class Item {
+  Item({required this.id, required this.record});
+
+  final String id;
+  final Map<String, dynamic> record;
+}
+
+class ItemThumbnail extends StatelessWidget {
+  const ItemThumbnail({
+    super.key,
+    required this.galleryItem,
+    required this.onTap,
+  });
+
+  final Item galleryItem;
+  final GestureTapCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return GestureDetector(
+      onTap: onTap,
+      child: Hero(
+        tag: galleryItem.id,
+        child: Card(
+          semanticContainer: true,
+          color: Colors.grey.shade200,
+          shadowColor: Colors.transparent,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: 1,
+                child: Image.network(
+                  galleryItem.record['thumb'],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                child: Text(
+                  galleryItem.record['headline'] ?? '',
+                  overflow: TextOverflow.ellipsis,
+                  style: textTheme.titleMedium?.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GalleryPhotoViewWrapper extends StatefulWidget {
+  GalleryPhotoViewWrapper({
+    this.loadingBuilder,
+    this.backgroundDecoration,
+    this.minScale,
+    this.maxScale,
+    this.initialIndex = 0,
+    required this.galleryItems,
+    this.scrollDirection = Axis.horizontal,
+  }) : pageController = PageController(initialPage: initialIndex);
+
+  final LoadingBuilder? loadingBuilder;
+  final BoxDecoration? backgroundDecoration;
+  final dynamic minScale;
+  final dynamic maxScale;
+  final int initialIndex;
+  final PageController pageController;
+
+  final List<Item> galleryItems;
+  final Axis scrollDirection;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GalleryPhotoViewWrapperState();
+  }
+}
+
+class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
+  late int currentIndex = widget.initialIndex;
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(icon: const Icon(Icons.delete), onPressed: () {}),
+            Text(
+              widget.galleryItems[currentIndex].record['headline'] ?? '',
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(
+                context,
+              ).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.w600),
+            ),
+            IconButton(icon: const Icon(Icons.edit), onPressed: () {}),
+          ],
+        ),
+        Expanded(
+          child: PhotoViewGallery.builder(
+            scrollPhysics: const BouncingScrollPhysics(),
+            builder: _buildItem,
+            itemCount: widget.galleryItems.length,
+            loadingBuilder: widget.loadingBuilder,
+            backgroundDecoration: widget.backgroundDecoration,
+            pageController: widget.pageController,
+            onPageChanged: onPageChanged,
+            scrollDirection: widget.scrollDirection,
+          ),
+        ),
+      ],
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildItem(BuildContext context, int index) {
+    final Item item = widget.galleryItems[index];
+    return PhotoViewGalleryPageOptions(
+      imageProvider: NetworkImage(item.record['url'] as String),
+      initialScale: PhotoViewComputedScale.contained,
+      minScale: PhotoViewComputedScale.contained,
+      maxScale: 1, //PhotoViewComputedScale.covered * 4.1,
+      heroAttributes: PhotoViewHeroAttributes(tag: item.id),
+    );
+  }
+}
